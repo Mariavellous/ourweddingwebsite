608bfc1f15ee64732717ad572c08f5d27cb0c2f9
Mariavellous <mm.alcaide38@gmail.com>
2021-11-06 22:13:18 -0700

    loop through Q&A

---
+++ main.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template
+import requests
+
+app = Flask(__name__)
+
+qa_url = "https://api.npoint.io/b97c8cf96988a5eb4435"
+response = requests.get(qa_url)
+QA_ITEMS = response.json()
+
+
+# Home Page
+@app.route('/')
+def home():
+  return render_template('index.html')
+
+# Guests Q&A
+@app.route('/faqs')
+def blog_post():
+  return render_template('faqs.html', qa_items=QA_ITEMS)
+
+
+
+if __name__ == "__main__":
+  app.run(debug=True)
